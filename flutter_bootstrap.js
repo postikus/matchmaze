81852d969e10cb201fa4ea9362dab807d89b786f
postikus <postikus@users.noreply.github.com>
2025-04-19 19:07:35 +0000

    Deploy: Merge pull request #10 from postikus/refactor/ui-settings
    refactor: extract UI settings to separate file and center back button 5f9d2d534defb7a093ae4e00a8e44cc9af6e36c2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1914869375"
+    serviceWorkerVersion: "2014440471"
   }
 });
